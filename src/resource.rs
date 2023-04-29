4678b2cd089a918ea4ec8d89b55e1ae0da0ae6c4
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-04-29 13:01:49 +1000

    Add comments

--- src/resource.rs
+++ src/resource.rs
@@ -39,18 +39,26 @@ pub(crate) fn read_resource_map(path: &Path) -> Result<Vec<ResourceMapEntry>, io
 
     loop {
         let rec = &buffer[idx..idx + 6];
+
+        // 0xffff ffff ffff marks the end of the resource map
+        // These should be the last 6 bytes of the file
         if rec.iter().all(|&x| x == 0xff) {
             break;
         }
 
-        // TODO: handle errors in unwrap
-        let first = u16::from_le_bytes(rec[0..2].try_into().unwrap());
-        let resource_type: ResourceType = FromPrimitive::from_u16(first >> 11).unwrap();
-        let resource_number = first & 0x7ff;
+        // TODO: handle errors in invalid resource types
 
-        let second: u32 = u32::from_le_bytes(rec[2..6].try_into().unwrap());
-        let resource_file_number = (second >> 26) as u8;
-        let resource_file_offset = second & 0x3ffffff;
+        let value = u16::from_le_bytes(rec[0..2].try_into().unwrap());
+        // 5 bit number of the resource type
+        let resource_type: ResourceType = FromPrimitive::from_u16(value >> 11).unwrap();
+        // 11 bit resource number
+        let resource_number = value & 0x7ff;
+
+        let value = u32::from_le_bytes(rec[2..6].try_into().unwrap());
+        // 8 bit number of the resource file containing the resource
+        let resource_file_number = (value >> 26) as u8;
+        // 24 bit number of the byte offset of the resource within the resource file
+        let resource_file_offset = value & 0x3ffffff;
 
         entries.push(ResourceMapEntry {
             resource_type,
