f0cca3b2d3df63585d8454b306495139b9a675d2
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-04-29 12:44:48 +1000

    Load resource map

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,36 @@
 use std::path::Path;
 
+#[macro_use]
+extern crate num_derive;
+
 mod resource;
 
-pub struct Game {}
+pub struct Game<'a> {
+    game_path: &'a Path,
+    resource_map: Vec<resource::ResourceMapEntry>,
+}
 
-impl Game {
+impl Game<'_> {
     pub fn load(path: &str) -> Result<Game, std::io::Error> {
         let game_path = Path::new(path);
 
         // TODO: possible error handling if it doesn't exist
 
-        let game = Game {};
+        let resource_map = resource::read_resource_map(&game_path.join("RESOURCE.MAP").as_path())?;
 
-        todo!("Read resource.map");
+        let game = Game {
+            game_path,
+            resource_map,
+        };
 
         Ok(game)
     }
 
     pub fn run(self: Self) {
-        todo!("Render an image from the game to start with");
+        // TODO: move specifics to examples directory instead
+
+        // TODO: use the resource library to
+        // load resources from a map entry
+        // Render an image from the game to start with
     }
 }
--- src/resource.rs
+++ src/resource.rs
@@ -1,26 +1,66 @@
+use std::{io, path::Path};
+
+use num_traits::FromPrimitive;
+
 mod decompress;
 
+#[derive(FromPrimitive, Debug)]
 enum ResourceType {
-    View = 0,
-    Pic = 1,
-    Script = 2,
-    Text = 3,
-    // Start simple, other types later
+    View,
+    Pic,
+    Script,
+    Text,
+    Sound,
+    Memory,
+    Vocab,
+    Font,
+    Cursor,
+    Patch,
+    Bitmap,
+    Palette,
+    // TODO: add other types later
 }
 
-struct ResourceMapEntry {
-    resourceType: ResourceType,
-    resourceNumber: u16,
-    resourceFileNumber: u8,
-    resourceFileOffset: u32,
+pub(crate) struct ResourceMapEntry {
+    resource_type: ResourceType,
+    resource_number: u16,
+    resource_file_number: u8,
+    resource_file_offset: u32,
 }
 
-struct ResourceMap {
-    entries: Vec<ResourceMapEntry>,
-}
-
-fn read_resource_map() -> ResourceMap {
+pub(crate) fn read_resource_map(path: &Path) -> Result<Vec<ResourceMapEntry>, io::Error> {
     // TODO: Integration tests for this using game data that isn't checked in
 
-    ResourceMap { entries: vec![] }
+    // Read SCI0 resource.map file
+
+    let buffer = std::fs::read(path)?;
+    let mut idx = 0;
+    let mut entries: Vec<ResourceMapEntry> = Vec::new();
+
+    loop {
+        let rec = &buffer[idx..idx + 6];
+        if rec.iter().all(|&x| x == 0xff) {
+            break;
+        }
+
+        // TODO: handle errors in unwrap
+        let first = u16::from_le_bytes(rec[0..2].try_into().unwrap());
+        let resource_type: ResourceType = FromPrimitive::from_u16(first >> 11).unwrap();
+        let resource_number = first & 0x7ff;
+
+        let second: u32 = u32::from_le_bytes(rec[2..6].try_into().unwrap());
+        let resource_file_number = (second >> 26) as u8;
+        let resource_file_offset = second & 0x3ffffff;
+
+        entries.push(ResourceMapEntry {
+            resource_type,
+            resource_number,
+            resource_file_number,
+            resource_file_offset,
+        });
+
+        idx += 6;
+    }
+
+    Ok(entries)
 }
